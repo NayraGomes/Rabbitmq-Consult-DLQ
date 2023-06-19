29735a6d2f6ec35c81ea8901355da995ce62d440
Nayra Neves <ngn.eng16@uea.edu.br>
2023-06-19 16:32:55 -0400

    script v1 in js

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # Rabbitmq-Consult-DLQ
 Script to manage a RabbitMQ's Dead-Letter Queue via CLI
+
+# RabbitMQ DLQs
+
+Script para manuseio das filas de mensagens mortas (DLQ) CLI, onde outras filas (filas de origem) usam como destinos para mensagens que não podem ser processadas (consumidas) com êxito.
+
+## 🚀 Começando
+
+Essas instruções permitirão que você obtenha uma cópia do projeto em operação na sua máquina local para fins de desenvolvimento e teste.
+
+Consulte **Implantação** para saber como implantar o projeto.
+
+### 📋 Pré-requisitos
+
+- Node.js (v16.16.0 ou superior)
+- Npm (v8.11.0 ou superior)
+- Bibliotecas do Node.js: amqplib e readline-sync.
+
+
+### 🔧 Instalação
+
+1. Instale o Node.js na sua máquina através de <https://nodejs.org/en/download/>
+2. As ultimas versões ja inlcuem o gerenciador de pacotes npm para o Node.JS certifique-se de consultar a versão de ambos.
+3. Instale as bibliotecas amqplib e readline-sync via linha de comando atras de um CLI da sua preferencia;
+
+```
+npm install amqplib
+```
+E repita para a próxima biblioteca
+
+```
+npm install readline-sync
+```
+
+## ⚙️ Funcionamento
+
+# Clone esse repositório:
+$ git clone <url_do_script_no_github>
+
+# Abra a pasta do projeto e edite o arquivo info.json
+Use o editor de txt da sua preferência e altere os campos queue, exchange e exchange_retry para as informações da fila de mensagens mortas que deseja consultar.
+
+```
+{
+	"queue": "<nome_da_fila_morta>",
+	"exchange": "<nome_da_exchange_dle>",
+	"exchange_retry": "<nome_da_exchange_reply>"
+}
+
+```
+Salve o arquivo e abra o CLI.
+
+# Navegue para o diretório raiz do projeto
+$ cd RabbitMQ-consult-DLQ
+
+#Execute o script de consult-DLQ
+
+$ node consult-DLQ
+
+Após esse comando o script será inicializado.
+
+
+### ⌨️ Como usar
+
+##Informações da fila
+
+Após inicialização, será mostrado as informações gerais da fila com nome, quantidade de consumidores e quantidade de mensagens.
+Se o número de mensagens for 0, aperte CTRL + C para sair.
+
+##Mensagens
+
+Se houver mensagens na fila será exibido na interface uma mensagem por vez por no qual você deverá escolher entre excluir a mensagem da fila ou reenvia-la para o fluxo principal.
+Após exclusão ou reenviou, se houver uma nova mensagem na fila, está será exibida na tela e o fluxo continuará para as demais.
+
+
+## 🛠️ Construído com
+
+
+* [JavaScript](https://www.javascript.com/) - Linguagem utilizada
+* [Node.JS](https://nodejs.org/en/download/ - Software para execução Javascript
+* [Npm](https://www.npmjs.com/ - Gerenciador de Dependências nodejs
+* [amqplib](https://amqp-node.github.io/amqplib/channel_api.html) - Client API do rabbitmq para nodejs
+
+
+## ✒️ Autores
+
+* **Nayra Gomes Neves** - *Trabalho inicial e desenvolvimento* - [Arquitetura de Soluções]
+
+
+## 🎁 Expressões de gratidão
+
+* Conte a outras pessoas sobre este projeto 📢
+* Obrigado publicamente 🤓.
+* etc.
+
+
+---
+
---
+++ consult-DLQ-v1.js
@@ -0,0 +1,159 @@
+#!/usr/bin/env node
+var amqp = require('amqplib/callback_api');
+var readlineSync = require('readline-sync');
+var fs = require("fs");
+ 
+
+var jsonData = JSON.parse(fs.readFileSync("./info.json", "utf8")); //Pega informações de fila e exchanges no arquivo info.json
+//Declaração das variaveis de fila
+var queue = jsonData.queue;
+var exchange = jsonData.exchange;
+var exchange_retry = jsonData.exchange_retry;
+
+
+function main (){
+
+    amqp.connect('<SERVER-ADDRESS', function(error0, connection) { //abrindo conexão
+        if (error0) {
+            throw error0;
+        }
+
+        function actions(){ //função actions para Exclusão e Reenvio de mensagens
+
+            var answer = readlineSync.question("Pressione (1) para Excluir, (2) para Reprocessar, CRTL + C para sair. ");
+
+            if(answer==1){ //Processo para Excluir uma mensagem da fila morta 'queue'
+                console.log("Excluindo mensagem...");
+                connection.createChannel(function(error1, channel) { //criação do canal
+                    if (error1) {throw error1;}
+                    channel.assertExchange(exchange, "fanout", {durable: true})
+                    channel.assertExchange(exchange_retry, "fanout", {durable: true})
+                    channel.assertQueue(queue, {durable: true, deadLetterExchange: exchange_retry,})
+                    channel.bindQueue(queue, exchange, 'chave-aleatoria');
+                    channel.get(queue, {noAck: false}, function(err, msgOrFalse) {
+                        channel.ack(msgOrFalse) //Ack na mensagem para excluir a mesma da fila
+                        console.log("Mensagem excluída!")
+                        console.log(" ")
+                        console.log(" ")
+                        console.log("Próxima mensagem...")
+                        setTimeout(function(){
+                            exibirMensagens(); //chama a função exibirMensagens para exibir a proxima mensagem
+                        },2000); 
+                            });
+                        });
+                }
+                else if(answer==2){ //Processo para reenviar a mensagem da fila morta 'queue' para a fila principal
+                    console.log("Reenviando mensagem");
+                    connection.createChannel(function(error1, channel) { //criação do canal
+                        if (error1) {throw error1;}
+                        channel.assertExchange(exchange, "fanout", {durable: true,})
+                        channel.assertExchange(exchange_retry, "fanout", {durable: true})
+                        channel.assertQueue(queue, {durable: true, deadLetterExchange: exchange_retry,})
+                        channel.bindQueue(queue, exchange, 'chave-aleatoria');
+                        channel.get(queue, {noAck: false}, function(err, msgOrFalse) {
+                            channel.reject(msgOrFalse, false) //Reject com requeue 'false' na mensagem para excluir a mesma da fila
+                            console.log("Mensagem reenviada!")
+                            console.log(" ")
+                            console.log(" ")
+                            console.log("Próxima mensagem...")
+                            setTimeout(function(){
+                                exibirMensagens(); //chama a função exibirMensagens para exibir a proxima mensagem
+                            },2000);
+                        });
+                    });                                
+                }
+                else{ //Processo caso o usuário não digite nem (1) nem (2)
+                    console.log("Inválido")
+                    setTimeout(function(){
+                        exibirMensagens(); //chama a função exibirMensagens para exibir a mensagem atual novamente
+                    },2500)           
+                }
+        } //fim da função actions()
+
+        function exibirMensagens(){ //função para exibir mensagens da fila uma a uma
+            connection.createChannel(async function(error1, channel) { //criação do canal
+                if (error1) {throw error1;}
+                channel.assertExchange(exchange, "fanout", {durable: true})
+                channel.assertExchange(exchange_retry, "fanout", {durable: true})
+                channel.assertQueue(queue, {durable: true, deadLetterExchange: exchange_retry})
+                channel.bindQueue(queue, exchange, 'chave-aleatoria');
+                channel.checkQueue(queue, async function(err, ok){
+
+                    if(ok.messageCount==0){ //Verifica se a fila está vazia
+                        console.log("Fila está vazia.")
+                        console.log("Pressione CRTL+C para sair")
+                    }
+                    else{  //caso a fila não esteja vazia, o script pegará as mensagens para exibir (uma por vez)
+                        channel.get(queue, {noAck: false}, function(err, msgOrFalse) {
+                            var nro_mensagem=ok.messageCount-msgOrFalse.fields.messageCount;
+                            console.log("");
+                            console.log("Message: %d", nro_mensagem);
+                            console.log("O servidor reportou %d mensagens restantes.", msgOrFalse.fields.messageCount);
+                            console.log(" ");
+                            console.log(" ##########     Informações gerais ################");
+                            console.log(msgOrFalse.fields);
+                            console.log(" ##########         Headers        ################");
+                            console.log(msgOrFalse.properties.headers);
+                            console.log(" ##########         Payload        ################");
+                            console.log(msgOrFalse.content.toString());
+                            console.log(" ");
+                            console.log(" ");
+                            console.log("Ações ");
+                            actions(); //chama a função actions() para exibir o menu de exclusão e reenvio. 
+                            channel.nackAll(msgOrFalse, false) //Nack com requeue 'false' na mensagem para apenas exibir a mesma sem altera-la
+                        }
+                        );  
+                    }
+                })
+            });
+        }//fim da função exibirMensagens()
+
+        function iniciar(){  //função para exibir as informaçõe iniciais da fila como nome da fila, qtdd de consumidores e qtt d emensagens
+            connection.createChannel(async function(error1, channel) { //criação do canal
+                if (error1) {throw error1;}
+                channel.assertExchange(exchange, "fanout", {durable: true})
+                channel.assertExchange(exchange_retry, "fanout", {durable: true})
+                channel.assertQueue(queue, {durable: true, deadLetterExchange: exchange_retry})
+                channel.bindQueue(queue, exchange, 'chave-aleatoria');  
+                channel.checkQueue(queue, async function(err, ok){ //checkQueue é responsavel por pegar as informações da fila. Devolve um array com 3 informações
+                    console.log("Inicializando..."); 
+                    setTimeout(function(){
+                        console.log("...");
+                    },1000);  
+                    setTimeout(function(){
+                        console.log("...");
+                    },2000);
+                    setTimeout(function(){
+                        console.log("...");
+                    },3000);
+                    setTimeout(function(){
+                        console.log(" ");
+                        console.log(" ");
+                        console.log("Informações da fila ");
+                        console.log(" ");
+                        console.log("Nome da Fila: ", ok.queue); //printa o nome da fila
+                        console.log("Quantidade de Consumidores: ", ok.consumerCount); //printa a quantidade de consumidores da fila
+                        console.log("Quantidade de Mensagens: ", ok.messageCount); //printa a quantidade de mensagens da fila
+                        console.log(" ");
+                        console.log(" ");
+                    },5000);
+                    setTimeout(function(){
+                        if(ok.messageCount==0){ //caso a fila esteja vazia
+                            console.log("A Fila está vazia.")
+                            console.log("Pressione CRTL+C para sair")
+                        }
+                        else{ 
+                            exibirMensagens(); //caso a fila tenha mensagens, exibirMensagens será chamado
+                        }
+                    },6000);
+                });
+            });           
+        } //fim da função iniciar()
+
+        iniciar(); //chama a função iniciar para começar o fluxo do script
+
+    })//fechamento da conexão amqp com o servidor
+
+}//fim da função main
+
+main();//chamando a função principal main()
